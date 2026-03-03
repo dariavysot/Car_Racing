6b61a8939d56d884d5e5e174f26df9a8187b006c
BondarchukLiza <blizaveta7@gmail.com>
2026-03-03 03:08:39 +0200

    refactor: update PlayerCar to support custom key bindings and movement direction

--- entities/player.py
+++ entities/player.py
@@ -3,17 +3,25 @@ from config import Settings as C
 from core.game_object import GameObject
 
 class PlayerCar(GameObject):
-    def __init__(self, image):
+    def __init__(self, image, left_key=pg.K_LEFT, right_key=pg.K_RIGHT):
         start_x = C.WIDTH // 2
         start_y = C.PLAYER_Y
 
         super().__init__(image, start_x, start_y)
         self.speed = C.PLAYER_LANE_SPEED
+        self.left_key = left_key
+        self.right_key = right_key
+        self.direction = 0
 
     def update(self, keys):
-        if keys[pg.K_LEFT]:
+        self.direction = 0
+
+        if keys[self.left_key]:
             self.rect.x -= self.speed
-        if keys[pg.K_RIGHT]:
+            self.direction = -1
+
+        if keys[self.right_key]:
             self.rect.x += self.speed
+            self.direction = 1
 
         self.rect.x = max(0, min(self.rect.x, C.WIDTH - self.rect.width))
