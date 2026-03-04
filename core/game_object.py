241666a63532b53cb9d8c5f794db2a394fcac55c
xvysot00 <xvysot00@vutbr.cz>
2026-03-04 17:47:57 +0200

    refactor(effect): move light rendering logic to GameObject base class

--- core/game_object.py
+++ core/game_object.py
@@ -14,3 +14,51 @@ class GameObject:
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def draw_lights(self, screen, is_night, direction="SAME"):
+        if not is_night:
+            return
+
+        # --- 1. HEADLIGHTS (Cone of light) ---
+        light_height = 220
+        top_width = int(self.rect.width * 0.6)
+        bottom_width = int(self.rect.width * 1.6)
+
+        light_surf = pg.Surface((bottom_width, light_height), pg.SRCALPHA)
+        center_x = bottom_width // 2
+        main_color = (255, 255, 200)
+
+        for y in range(light_height):
+            progress = y / light_height
+
+            alpha = int(130 * (1 - progress) ** 1.5)
+            current_width = top_width + (bottom_width - top_width) * progress
+
+            pg.draw.line(
+                light_surf, 
+                (*main_color, alpha), 
+                (int(center_x - current_width/2), light_height - y), 
+                (int(center_x + current_width/2), light_height - y)
+            )
+
+        if direction == "OPPOSITE":
+            flipped_light = pg.transform.flip(light_surf, False, True)
+            screen.blit(flipped_light, (self.rect.centerx - bottom_width // 2, self.rect.bottom - 5))
+        else:
+            screen.blit(light_surf, (self.rect.centerx - bottom_width // 2, self.rect.top - light_height + 5))
+
+        # --- 2. REAR RED LIGHTS ---
+        # Позиція залежить від напрямку (протилежна переднім фарам)
+        back_y = self.rect.top + 3 if direction == "OPPOSITE" else self.rect.bottom - 3
+        
+        dot_radius = 2
+        glow_radius = 5
+        
+        for offset in [-self.rect.width // 3.5, self.rect.width // 3.5]:
+            pos = (int(self.rect.centerx + offset), int(back_y))
+            
+            pg.draw.circle(screen, (255, 100, 100), pos, dot_radius)
+            
+            glow_surf = pg.Surface((glow_radius * 2, glow_radius * 2), pg.SRCALPHA)
+            pg.draw.circle(glow_surf, (200, 0, 0, 30), (glow_radius, glow_radius), glow_radius)
+            screen.blit(glow_surf, (pos[0] - glow_radius, pos[1] - glow_radius), special_flags=pg.BLEND_RGB_ADD)
--- entities/obstacle.py
+++ entities/obstacle.py
@@ -23,47 +23,4 @@ class Obstacle(GameObject):
         super().draw(screen)
 
     def draw_only_light(self, screen, is_night):
-
-        if not is_night:
-            return
-
-        # --- 1. DRAWING THE HEADLIGHTS  ---
-        light_height = 220
-        top_width = int(self.rect.width * 0.6)
-        bottom_width = int(self.rect.width * 1.6)
-
-        light_surf = pg.Surface((bottom_width, light_height), pg.SRCALPHA)
-        center_x = bottom_width // 2
-
-        main_color = (255, 255, 200)
-
-        for y in range(light_height):
-            progress = y / light_height
-            alpha = int(120 * (1 - progress) ** 2)
-            current_width = top_width + (bottom_width - top_width) * progress
-
-            pg.draw.line(light_surf, (*main_color, alpha), 
-                         (int(center_x - current_width/2), light_height - y), 
-                         (int(center_x + current_width/2), light_height - y))
-
-        if self.direction == "OPPOSITE":
-            light_surf = pg.transform.flip(light_surf, False, True)
-            screen.blit(light_surf, (self.rect.centerx - bottom_width // 2, self.rect.bottom - 5))
-        else:
-            screen.blit(light_surf, (self.rect.centerx - bottom_width // 2, self.rect.top - light_height + 5))
-
-        # --- 2. DRAWING THE REAR RED LIGHTS ---
-        back_y = self.rect.top + 3 if self.direction == "OPPOSITE" else self.rect.bottom - 3
-
-        dot_radius = 2
-        glow_radius = 5
-
-        for offset in [-self.rect.width // 3.5, self.rect.width // 3.5]:
-            pos = (self.rect.centerx + offset, back_y)
-
-            pg.draw.circle(screen, (255, 100, 100), pos, dot_radius)
-
-            glow_surf = pg.Surface((glow_radius * 2, glow_radius * 2), pg.SRCALPHA)
-            pg.draw.circle(glow_surf, (200, 0, 0, 40), (glow_radius, glow_radius), glow_radius)
-
-            screen.blit(glow_surf, (pos[0] - glow_radius, pos[1] - glow_radius), special_flags=pg.BLEND_RGB_ADD)
+        self.draw_lights(screen, is_night, self.direction)
