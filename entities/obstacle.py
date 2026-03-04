29f9fb2cdd673f03b32fedead5e46cf231681bb5
xvysot00 <xvysot00@vutbr.cz>
2026-03-04 16:02:54 +0200

    feat(effects): add red taillights to all vehicles

--- entities/obstacle.py
+++ entities/obstacle.py
@@ -25,7 +25,7 @@ class Obstacle(GameObject):
         if not is_night:
             return
 
-        # --- DRAWING THE HEADLIGHTS  ---
+        # --- 1. DRAWING THE HEADLIGHTS  ---
         light_height = 220
         top_width = int(self.rect.width * 0.6)
         bottom_width = int(self.rect.width * 1.6)
@@ -49,3 +49,19 @@ class Obstacle(GameObject):
             screen.blit(light_surf, (self.rect.centerx - bottom_width // 2, self.rect.bottom - 5))
         else:
             screen.blit(light_surf, (self.rect.centerx - bottom_width // 2, self.rect.top - light_height + 5))
+
+        # --- 2. DRAWING THE REAR RED LIGHTS ---
+        back_y = self.rect.top + 3 if self.direction == "OPPOSITE" else self.rect.bottom - 3
+        
+        dot_radius = 2
+        glow_radius = 5
+        
+        for offset in [-self.rect.width // 3.5, self.rect.width // 3.5]:
+            pos = (self.rect.centerx + offset, back_y)
+
+            pg.draw.circle(screen, (255, 100, 100), pos, dot_radius)
+
+            glow_surf = pg.Surface((glow_radius * 2, glow_radius * 2), pg.SRCALPHA)
+            pg.draw.circle(glow_surf, (200, 0, 0, 80), (glow_radius, glow_radius), glow_radius)
+
+            screen.blit(glow_surf, (pos[0] - glow_radius, pos[1] - glow_radius), special_flags=pg.BLEND_RGB_ADD)
--- entities/player.py
+++ entities/player.py
@@ -24,7 +24,7 @@ class PlayerCar(GameObject):
         if not is_night:
             return
 
-        # --- DRAWING THE HEADLIGHTS  ---
+        # --- 1. DRAWING THE HEADLIGHTS  ---
         light_height = 220
         top_width = int(self.rect.width * 0.6)
         bottom_width = int(self.rect.width * 1.6)
@@ -53,3 +53,18 @@ class PlayerCar(GameObject):
             (self.rect.centerx - bottom_width // 2,
             self.rect.top - light_height + 5)
         )
+
+        # --- 2. DRAWING THE REAR RED LIGHTS ---
+        back_y = self.rect.bottom - 3
+        dot_radius = 2
+        glow_radius = 5
+        
+        for offset in [-self.rect.width // 3.5, self.rect.width // 3.5]:
+            pos = (int(self.rect.centerx + offset), back_y)
+            
+            pg.draw.circle(screen, (255, 100, 100), pos, dot_radius)
+            
+            glow_surf = pg.Surface((glow_radius * 2, glow_radius * 2), pg.SRCALPHA)
+            pg.draw.circle(glow_surf, (200, 0, 0, 80), (glow_radius, glow_radius), glow_radius)
+            
+            screen.blit(glow_surf, (pos[0] - glow_radius, pos[1] - glow_radius), special_flags=pg.BLEND_RGB_ADD)
